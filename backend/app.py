98204b07e779292bda8132ee86e7cc00ab0d3bd6
rayenealmeida <almeidarayene@gmail.com>
2025-02-10 22:23:00 -0300

    Config Frontend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -9,4 +13,4 @@ def home():
     return jsonify({"message": "Formômetro API está rodando!"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
