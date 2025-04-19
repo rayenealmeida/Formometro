46d0c27e7dcd38837e475bdd76e8e957d25046b9
rayenealmeida <almeidarayene@gmail.com>
2025-04-19 14:24:30 -0300

    refatorando

--- backend/routes/usuarios.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from models.usuario import Usuario
-
-usuarios_bp = Blueprint('usuarios', __name__)
-
-@usuarios_bp.route('/usuarios', methods=['GET'])
-def get_usuarios():
-    usuarios = Usuario.query.all()
-    return jsonify([{'id': u.id, 'nome': u.nome, 'email': u.email} for u in usuarios])
